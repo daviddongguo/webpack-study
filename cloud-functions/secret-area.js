5bb73821c9e8499986e40c3e20c29c00a297a7b2
daviddongguo <david.dong.guo@gmail.com>
2021-05-05 09:10:37 -0400

    hot fix

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,11 +1,4 @@
 exports.handler = function (event, context, callback) {
-	const secretContent = (
-		<div>
-			<h1>Secret Content</h1>
-			<p>hi, there!</p>
-		</div>
-	);
-
 	let body = {};
 	if (event.body) {
 		body = JSON.parse(event.body);
