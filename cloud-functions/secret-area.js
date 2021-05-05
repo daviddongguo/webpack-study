989eb9b8f2afe105de5bdd5494a24108629441af
daviddongguo <david.dong.guo@gmail.com>
2021-05-05 10:05:21 -0400

    update function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,30 @@
 exports.handler = function (event, context, callback) {
-	let body = {};
-	if (event.body) {
-		body = JSON.parse(event.body);
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Content-Type',
+	};
+
+	if (event.httpMethod !== 'POST') {
+		return callback(null, {
+			statusCode: 200,
+			headers,
+			body: 'This was not a POST request',
+		});
 	}
+
+	const secretContent = `
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `;
+
+	if (!event.body) {
+		callback(null, {
+			stausCode: 401,
+			body: `nothing is transmited`,
+		});
+	}
+
+	const body = JSON.parse(event.body);
 
 	if (body.password === '123') {
 		callback(null, {
@@ -10,6 +32,7 @@ exports.handler = function (event, context, callback) {
 			body: `password(${body.password}) is correct`,
 		});
 	}
+
 	callback(null, {
 		stausCode: 401,
 		body: `password(${body.password}) is wrong`,
