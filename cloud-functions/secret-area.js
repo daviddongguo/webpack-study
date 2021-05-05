3201eddeb9588195b2709104bdfb861858f5051d
daviddongguo <david.dong.guo@gmail.com>
2021-05-05 08:59:28 -0400

    Add input in function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,17 @@
 exports.handler = function (event, context, callback) {
+	let body = {};
+	if (event.body) {
+		body = JSON.parse(event.body);
+	}
+
+	if ((body.password = '123')) {
+		callback(null, {
+			stausCode: 200,
+			body: `password(${context.name}) is correct`,
+		});
+	}
 	callback(null, {
-		stausCode: 200,
-		body: 'Welcome to the super secret area',
+		stausCode: 400,
+		body: `password(${context.name}) is wrong`,
 	});
 };
